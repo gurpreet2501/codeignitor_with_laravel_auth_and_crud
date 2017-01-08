d48f85e127b14ddc78e17eb6afea4d69cc53b8c9
unknown <gurpreet2501@gmail.com>
2017-01-08 10:54:13 +0530

    empty laravel db crud tank auth with codeignitor

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+use App\Models;
+class Home extends MY_Controller
+{
+    public function index(){
+       
+        echo "<pre>";
+        print_r('$t');
+        exit;
+
+      
+    }
+
+   
+}
